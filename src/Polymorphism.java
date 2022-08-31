5808bd97072f02d72bc84fd243897c85d2f966bb
admin <eremin@yaclass.eu>
2022-08-31 18:00:45 +0300

    ДЗ 18. Полиморфизм
    1.Сделайте класс MusicStyles, который будет содержать метод playMusic()
    2.Сделайте его наследников: PopMusic, RockMusic, и ClassicMusic.
    3.Наследники должны по-своему реализовывать метод playMusic().
    4.В мейн-классе создайте музыкальные группы для каждого стиля
    5.С помощью метода for each вызовите у всех наследников MusicStyles метод playMusic()

--- src/Polymorphism.java
+++ src/Polymorphism.java
@@ -1,57 +1,73 @@
+import java.util.ArrayList;
+
 public class Polymorphism {
 
-// Сделайте класс MusicStyles, который будет содержать метод playMusic()
-    public class MusicStyles{
+    // Сделайте класс MusicStyles, который будет содержать метод playMusic()
+    public static class MusicStyles {
 
-    public static String groupName;
+        public String groupName;
 
-    MusicStyles (String groupName) {
-        this.groupName=groupName;
+        MusicStyles(String groupName) {
+            this.groupName = groupName;
 
-    }
-    void playMusic(){
-        System.out.println("﴾͡๏̯͡๏﴿");
-}
-//Сделайте его наследников: PopMusic, RockMusic, и ClassicMusic.
-        public class PopMusic extends MusicStyles {
-    PopMusic(String groupName) {
-        super(groupName);
+        }
+
+        void playMusic() {
+            System.out.println("﴾͡๏̯͡๏﴿");
+        }
     }
 
-    //Наследники должны по-своему реализовывать метод playMusic().
-            @Override
-            public void playMusic() {
-                System.out.println("(•_•)");
-            }
-        }
-        public class RockMusic extends MusicStyles {
-            RockMusic(String groupName) {
-                super(groupName);
-            }
-
-            @Override
-            public void playMusic() {
-                System.out.println("(ノಠ益ಠ)ノ彡┻━┻");
-            }
-        }
-    public class ClassicMusic extends MusicStyles {
-        ClassicMusic(String groupName) {
+    //Сделайте его наследников: PopMusic, RockMusic, и ClassicMusic.
+    public static class PopMusic extends MusicStyles {
+        PopMusic(String groupName) {
             super(groupName);
+            this.groupName = groupName;
+        }
+
+        //Наследники должны по-своему реализовывать метод playMusic().
+        @Override
+        public void playMusic() {
+            System.out.println(this.groupName);
+            System.out.println("(•_•)");
+        }
+    }
+
+    public static class RockMusic extends MusicStyles {
+        RockMusic(String groupName) {
+            super(groupName);
+            this.groupName = groupName;
         }
 
         @Override
         public void playMusic() {
+            System.out.println(this.groupName);
+            System.out.println("(ノಠ益ಠ)ノ彡┻━┻");
+        }
+    }
+
+    public static class ClassicMusic extends MusicStyles {
+        ClassicMusic(String groupName) {
+            super(groupName);
+            this.groupName = groupName;
+        }
+
+        @Override
+        public void playMusic() {
+            System.out.println(this.groupName);
             System.out.println("ヾ(⌐■_■)ノ♪");
         }
     }
 
-
-
-    }
-
+    public static void main(String[] args) {
 //В мейн-классе создайте музыкальные группы для каждого стиля
-    public void main(String[] args) {
-        MusicStyles justinBieber = new MusicStyles("Justin Bieber");
+        PopMusic justinBieber = new PopMusic("Justin Bieber");
+        RockMusic AcDc = new RockMusic("AC / DC");
+        ClassicMusic Bach = new ClassicMusic("Johann Sebastian Bach");
+
+//С помощью метода for each вызовите у всех наследников MusicStyles метод playMusic()   <--- это не понял как сделать
         justinBieber.playMusic();
+        AcDc.playMusic();
+        Bach.playMusic();
+
     }
 }
